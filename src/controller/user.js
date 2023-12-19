3226673b07a3514b5c1b4c821fcd9c364e9a175b
rioxiu <richardosinulingga@icloud.com>
2023-12-20 06:59:59 +0700

    feat: fix all repositories

--- src/controller/user.js
+++ src/controller/user.js
@@ -66,7 +66,10 @@ export const loginUser = async (req, res, next) => {
     });
 
     if (!findUser) {
-      return res.status(404).json({ message: "Akun tidak ada" });
+      return res.status(404).json({
+        message: "Akun tidak ada",
+        data: null,
+      });
     }
 
     //compare password input with hash
